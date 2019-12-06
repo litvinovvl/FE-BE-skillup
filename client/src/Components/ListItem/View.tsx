b9cad022547aac88048c1442c028d273f87e5947
Litvinov, Vlad [PS] <vlad.litvinov@cengage.com>
2019-12-06 16:49:06 +0200

    Add posibility to remove item from list

--- client/src/Components/ListItem/View.tsx
+++ client/src/Components/ListItem/View.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import { default as MUIListItem}  from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
 
-const ListItem = () => {
+const ListItem = ({ podcast, removePodcast }: any) => {
+  const deletePodcast = () => removePodcast({ variables: { input: { id: podcast.id } } });
+
   return (
-    <div>List item</div>
+    <MUIListItem key={podcast.id} button>
+      <ListItemAvatar>
+        <Avatar
+          src={podcast.thumbnail}
+        />
+      </ListItemAvatar>
+      <ListItemText primary={podcast.title} />
+      <ListItemSecondaryAction>
+        <IconButton aria-label="Delete" color="secondary" onClick={deletePodcast}>
+          <DeleteIcon />
+        </IconButton>
+      </ListItemSecondaryAction>
+    </MUIListItem>
   )
 }
 
--- client/src/Components/ListItem/index.ts
+++ client/src/Components/ListItem/index.ts
@@ -1,1 +1,22 @@
-export {default as ListItem} from './View';
+import gql from 'graphql-tag';
+import { compose, graphql } from 'react-apollo';
+import ListItem from './View';
+import { GET_PODCASTS } from '../List';
+
+export const REMOVE_PODCAST = gql`
+  mutation removePodcast ($input: RemovePodcastInput!) {
+    removePodcast (input: $input) {
+      id
+    }
+  }
+`;
+
+const removePodcast = graphql(REMOVE_PODCAST, {
+  name: 'removePodcast',
+  options: {
+    refetchQueries: () => [{ query: GET_PODCASTS }],
+    onCompleted: ({ removePodcast: { id } }: any) => console.log(id),
+  },
+});
+
+export default compose(removePodcast)(ListItem);
