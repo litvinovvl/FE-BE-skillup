697489a9185aa9bb06d6666febfbeebafe4ddd75
Litvinov, Vlad [PS] <vlad.litvinov@cengage.com>
2019-12-26 16:16:30 +0200

    use .env

--- server/db/index.ts
+++ server/db/index.ts
@@ -2,11 +2,11 @@ import { createConnection, Connection } from "typeorm";
 
 const connection = async (): Promise<Connection> => await createConnection({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "litvinov",
-    password: "kZbYJ77G",
-    database: "podcastsdb",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     entities: [
       "db/entity/*.ts"
     ],
